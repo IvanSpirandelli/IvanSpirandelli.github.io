44a205af2151005675bf6773b7244d34d795cd6d
Ivan Spirandelli <ivan@spirandelli.de>
2025-10-07 17:20:06 -0700

    new layout

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let slideIndex = 0;
-showSlide(slideIndex);
+document.addEventListener("DOMContentLoaded", function() {
+    const collapsibles = document.getElementsByClassName("collapsible");
 
-// Next/previous controls
-function moveSlide(n) {
-    showSlide(slideIndex += n);
-}
-
-function showSlide(n) {
-    const slides = document.getElementsByClassName("slide");
-    if (n >= slides.length) { slideIndex = 0; }
-    if (n < 0) { slideIndex = slides.length - 1; }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active");
+    for (let i = 0; i < collapsibles.length; i++) {
+        collapsibles[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
     }
-    slides[slideIndex].classList.add("active");
-}
+});
